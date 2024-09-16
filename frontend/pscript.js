f9caeea9b21dedb809c2311c85f959657965692e
rajibmondal200 <135255220+rajibmondal200@users.noreply.github.com>
2024-09-16 21:50:07 +0530

    Delete frontend directory

--- frontend/pscript.js
+++
@@ -1,9 +0,0 @@
-// Function to toggle the dropdown menu
-function toggleMenu() {
-  var dropdown = document.getElementById('dropdown');
-  if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-    dropdown.style.display = 'block';
-  } else {
-    dropdown.style.display = 'none';
-  }
-}
